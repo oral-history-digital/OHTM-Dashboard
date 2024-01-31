c0a4a439aa6aaa0484d33ae02a56c2dd55aaf65c
PhilFAU <philipp.bayerschmidt@fau.de>
2024-01-31 10:00:29 +0100

    BA Update

--- ZWA_OHDdash.py
+++ ZWA_OHDdash.py
@@ -48,8 +48,6 @@ sidebar = html.Div(
                 dbc.NavLink("Dash-Board", href="/", active="exact"),
                 dbc.NavLink("Text Search", href="/page-1", active="exact"),
                 dbc.NavLink("Balkendiagram", href="/page-2", active="exact"),
-                dbc.NavLink("Chronology Heamtap", href="/page-3", active="exact"),
-                dbc.NavLink("Topic Übersicht", href="/page-4", active="exact"),
             ],
             vertical=True,
             pills=True,
@@ -90,29 +88,12 @@ def render_page_content(pathname):
                     dcc.Dropdown(id="slct_archiv",
                                  options=[
                                      {"label": "Archiv Zwangsarbeit", "value": "ZWA"},
-                                     {"label": "Archiv Deutsches Gedächtnis", "value": "ADG"},
-                                     {"label": "Werkstatt der Erinnerung", "value": "WdE"},
-                                     {"label": "Museum Friedland", "value": "MFL"},
-                                     {"label": "Flucht Vertreibung Versöhnung", "value": "FVV"},
-                                     {"label": "Hannah-Arendt-Institut", "value": "HAI"},
                                      {"label": "Gesamtkorpus", "value": "all"},
                                  ],
                                  multi=False,
                                  value="all",
                                  style={'width': "80%"}
                                  )], width=4),
-                dbc.Col([
-                    dbc.Checklist(
-                        options=[
-                            {"label": "Z Score", "value": "z_score"},
-                        ],
-                        value=[],
-                        id="switch_z_score_global_heatmap",
-                        switch=True,
-                    ),
-
-
-                ], width = 1)
             ]),
 
             dbc.Row([
@@ -132,35 +113,16 @@ def render_page_content(pathname):
                 dbc.Col([
                     html.H5([dbc.Badge(id="sent_titel", color="danger")], className="text-center")
                 ], width=5),
-                dbc.Row([
-                    dbc.Col([
-                        dbc.Checklist(
-                            options=[
-                                {"label": "Topic Filter", "value": "filter"},
-                                {"label": "Z Score", "value": "z_score"}
-                            ],
-                            value=[],
-                            id="switch_chronology_filter",
-                            switch=True,
-                            inline = True
-                        ),
-
-                    ], width =3),
-                    dbc.Col([
-                        html.Div(dbc.Input(id='interview_manual_id', placeholder="Interview",type='word')),
-
-                    ], width=1),
-                    dbc.Col([
-                        html.Div(dbc.Input(id='threshold_top_filter_value', placeholder="Top Filter Threshold", type='number')),
-
-                    ], width = 1),
-                    dbc.Col([
-                        html.Div(dbc.Input(id='outlier_threshold_value', placeholder="Outlier Threshold",
-                                           type='number')),
-                    ], width=1)
-                ]),
 
             ]),
+            dbc.Row([
+                dbc.Col([
+                    html.Div(dbc.Input(id='interview_manual_id', placeholder="Interview", type='word')),
+
+                ], width=1),
+
+            ]),
+
             dbc.Row([
                 dbc.Col([
                     dcc.Graph(id='heat_map_interview', figure={})
@@ -231,56 +193,6 @@ def render_page_content(pathname):
                 ]), ])
         ]
 
-    elif pathname == "/page-3":
-        return [
-            dbc.Row([
-                dbc.Col([
-                    html.H5([dbc.Badge(id="interview_titel_detail", color="danger")], className="text-center")
-                ], width=6),
-                dbc.Col([
-                    html.H5([dbc.Badge(id="sent_titel_detail", color="danger")], className="text-center")
-                ], width=5),
-                dbc.Row([
-                    dbc.Col([
-                        dbc.Checklist(
-                            options=[
-                                {"label": "Topic Filter", "value": "filter"},
-                                {"label": "Z Score", "value": "z_score"}
-                            ],
-                            value=[],
-                            id="switch_chronology_filter",
-                            switch=True,
-                            inline=True
-                        ),
-
-                    ], width=3),
-                    dbc.Col([
-                        html.Div(dbc.Input(id='interview_manual_id_detail', placeholder="Interview", type='word')),
-
-                    ], width=1),
-                    dbc.Col([
-                        html.Div(dbc.Input(id='threshold_top_filter_value_detail', placeholder="Top Filter Threshold",
-                                           type='number')),
-
-                    ], width=1),
-                    dbc.Col([
-                        html.Div(dbc.Input(id='outlier_threshold_value_detail', placeholder="Outlier Threshold",
-                                           type='number')),
-                    ], width=1)
-                ]),
-
-            ]),
-            dbc.Row([
-                dbc.Col([
-                    dcc.Graph(id='heat_map_interview_detail', figure={})
-                ], width=6),
-                dbc.Col([
-                    dbc.Row([
-                        html.Div(id='textarea_detail', style={'whiteSpace': 'pre-line'}),
-                    ]),
-                ], width=5),
-            ]),
-                ]
 
     # If the user tries to reach a different page, return a 404 message
     return dbc.Jumbotron(
@@ -295,14 +207,10 @@ def render_page_content(pathname):
 @app.callback(
     Output(component_id='heat_map', component_property='figure'),
     Input(component_id='slct_archiv', component_property='value'),
-    Input("switch_z_score_global_heatmap", "value")
 )
-def update_graph(value, z_score_global):
-    if "z_score" in z_score_global:
-        z_score = True
-    else: z_score = False
+def update_graph(value):
 
-    fig = heatmap_corpus(top_dic, option_selected=str(value),z_score = z_score,show_fig=False, return_fig=True)
+    fig = heatmap_corpus(top_dic, option_selected=str(value),z_score = False,show_fig=False, return_fig=True)
     return fig
 
 
@@ -311,29 +219,10 @@ def update_graph(value, z_score_global):
     Output(component_id='heat_map_interview', component_property='figure'),
     Output("interview_titel", "children"),
     Input("heat_map", "clickData"),
-    Input("switch_chronology_filter", "value"),
-    Input("threshold_top_filter_value", "value"),
-    Input("outlier_threshold_value", "value"),
     Input("interview_manual_id", "value"),
 )
-def interview_heat_map(clickData, heatmap_filter, top_filter_th, outlier_th, interview_manual_id):
+def interview_heat_map(clickData, interview_manual_id):
     global interview_id
-    global chronology_df
-    global tc_indicator
-
-
-    if "filter" in heatmap_filter:
-        topic_filtering = True
-    else: topic_filtering = False
-
-    if "z_score" in heatmap_filter:
-        z_score = True
-    else: z_score = False
-
-    if top_filter_th == None:
-        top_filter_th = 0.01
-    if outlier_th == None:
-        outlier_th = 0.02
 
     if interview_manual_id is not None:
         interview_id = interview_manual_id
@@ -342,12 +231,15 @@ def interview_heat_map(clickData, heatmap_filter, top_filter_th, outlier_th, int
     else:
         interview_id = clickData["points"][0]["y"]
 
-    chronology_data = chronology_matrix(top_dic, interview_id, return_fig=True, print_fig=False, z_score = z_score, topic_filter = topic_filtering, threshold_top_filter=top_filter_th, outlier_threshold=outlier_th)
-    chronology_df = chronology_data[1]
-    tc_indicator = chronology_data[2]
-    fig = chronology_data[0]
-    titel = "Interview chronology " + interview_id
+    dff = {}
+    for chunks in top_dic["weight"][interview_id[0:3]][interview_id]:
+        dff[chunks] = top_dic["weight"][interview_id[0:3]][interview_id][chunks]
 
+    df = pd.DataFrame.from_dict(dff)
+    df.index = pd.to_numeric(df.index)
+    titel = "Heatmap Interview: " + interview_id
+    fig = px.imshow(df, color_continuous_scale='deep')
+    fig.update_traces(hovertemplate="Chunk: %{x}" "<br>Topic: %{y}" "<br>Weight: %{z}<extra></extra>")
     return fig, titel
 
 
@@ -359,14 +251,7 @@ def interview_heat_map(clickData, heatmap_filter, top_filter_th, outlier_th, int
 )
 def sent_drawing(clickData):
 
-    #chunk_id = clickData["points"][0]["x"]
-
-    if tc_indicator:
-        time_id = clickData["points"][0]["x"]
-        row_index = chronology_df.index.get_loc(chronology_df[chronology_df["minute"] == time_id].index[0]) # die Information aus dem DF aus Chronology. Hier wird die Zeit und das zugehörige DF gespeichert. Wir müssen zunächst den Index der Zeitangabe finden
-        chunk_id = chronology_df.loc[row_index]["ind"] # mit dem Index der Zeitangabe kann hier der Chunkwert ausgelesen werden und als chunk_id übergeben werden
-    else:
-        chunk_id = clickData["points"][0]["x"]
+    chunk_id = clickData["points"][0]["x"]
 
     sent_example = []
     speaker = "None"
@@ -471,46 +356,6 @@ def weight_print(topic_print, weight_print, n_clicks):
         columns = [{"name": i, "id": i} for i in df_rounded.columns]
 
         return data, columns
-
-    # Heatmap Chronology Heatmap Detail
-    @app.callback(
-        Output(component_id='heat_map_interview_detail', component_property='figure'),
-        Output("interview_titel_detail", "children"),
-        Input("switch_chronology_filter_detail", "value"),
-        Input("threshold_top_filter_value_detail", "value"),
-        Input("outlier_threshold_value_detail", "value"),
-        Input("interview_manual_id_detail", "value"),
-    )
-    def interview_heat_map(heatmap_filter, top_filter_th, outlier_th, interview_manual_id):
-
-        if "filter" in heatmap_filter:
-            topic_filtering = True
-        else:
-            topic_filtering = False
-
-        if "z_score" in heatmap_filter:
-            z_score = True
-        else:
-            z_score = False
-
-        if top_filter_th == None:
-            top_filter_th = 0.01
-        if outlier_th == None:
-            outlier_th = 0.02
-
-        if interview_manual_id is not None:
-            interview_id = interview_manual_id
-
-        chronology_data = chronology_matrix(top_dic, interview_id, return_fig=True, print_fig=False, z_score=z_score,
-                                            topic_filter=topic_filtering, threshold_top_filter=top_filter_th,
-                                            outlier_threshold=outlier_th)
-        chronology_df = chronology_data[1]
-        tc_indicator = chronology_data[2]
-        fig = chronology_data[0]
-        titel = "Interview chronology " + interview_id
-
-        return fig, titel
-
 
 if __name__ == '__main__':
     app.run_server(debug=False, port=3002)
