f43b74b61a5261931103c493eedf8a12bac03df9
PhilFAU <105867477+bayerschphi@users.noreply.github.com>
2024-02-20 19:57:03 +0100

    navigate Text-chunk

--- OHDdash.py
+++ OHDdash.py
@@ -47,6 +47,9 @@ sidebar = html.Div(
         dcc.Store(id="bar_topic_nr", data="", storage_type="session"),
         dcc.Store(id="bar_detail_topic_nr", data="", storage_type="session"),
         dcc.Store(id="heatmap_corpus_topic_nr", data="", storage_type="session"),
+        dcc.Store(id="chunk_number_frontpage", data="", storage_type = "sesssion"),
+        dcc.Store(id="chunk_number_detail", data="", storage_type="sesssion"),
+
 
         html.P(id='placeholder'),
         html.Img(src=b64_image(image_filename), style={"max-width": "100%"}),
@@ -138,8 +141,14 @@ def render_page_content(pathname):
                     html.H5([dbc.Badge(id="interview_titel", color="danger")], className="text-center")
                 ], width=6),
                 dbc.Col([
-                    html.H5([dbc.Badge(id="sent_titel", color="danger")], className="text-center")
-                ], width=5),
+
+                    dbc.Button("<", id="-_button_frontpage", color="danger", size="sm"),
+
+                    dbc.Badge("chunk", id="sent_titel", color="danger"),
+
+                    dbc.Button(">", id="+_button_frontpage", color="danger", size = "sm")
+                ], className ="d-grid gap-2 d-md-block",
+                     width = 6),
                 dbc.Row([
                     dbc.Col([
                         dbc.Checklist(
@@ -284,7 +293,11 @@ def render_page_content(pathname):
             dbc.Row([
                     dcc.Graph(id='heat_map_interview_detail', figure={}),
             dbc.Row([
-                html.H5([dbc.Badge(id="sent_title_detail", color="danger")], className="text-center")
+                dbc.Col([
+                    dbc.Button("<", id="-_button_detail", color="danger", size="sm"),
+                    dbc.Badge("chunk", id="sent_title_detail", color="danger", className="text-center"),
+                    dbc.Button(">", id="-_button_detail", color="danger", size="sm"),
+                 ]),
             ]),
             dbc.Row([
                     html.Div(id='textarea_detail', style={'whiteSpace': 'pre-line'}),
@@ -326,6 +339,8 @@ def update_graph(value, z_score_global):
     Input("threshold_top_filter_value", "value"),
     Input("outlier_threshold_value", "value"),
     Input("interview_manual_id", "value"),
+    prevent_initial_call = True
+
 )
 def interview_heat_map(clickData, heatmap_filter, top_filter_th, outlier_th, interview_manual_id):
     global interview_id
@@ -362,22 +377,31 @@ def interview_heat_map(clickData, heatmap_filter, top_filter_th, outlier_th, int
     return fig, titel
 
 
-# Print der einzelnen Sätze des ausgewählten Chunks
+# Ausgabe der Raw-Sätze des Chunks
 @app.callback(
     Output(component_id='textarea', component_property='children'),
     Output("sent_titel", "children"),
+    Output("chunk_number_frontpage", "data"),
     Input("heat_map_interview", "clickData"),
+    Input("-_button_frontpage", "n_clicks"),
+    Input("+_button_frontpage", "n_clicks"),
+    State("chunk_number_frontpage", "data"),
+    prevent_initial_call=True
 )
-def sent_drawing(clickData):
+def sent_drawing(clickData, input_before, input_next, chunk_number):
 
-    #chunk_id = clickData["points"][0]["x"]
-
-    if tc_indicator:
-        time_id = clickData["points"][0]["x"]
-        row_index = chronology_df.index.get_loc(chronology_df[chronology_df["minute"] == time_id].index[0]) # die Information aus dem DF aus Chronology. Hier wird die Zeit und das zugehörige DF gespeichert. Wir müssen zunächst den Index der Zeitangabe finden
-        chunk_id = chronology_df.loc[row_index]["ind"] # mit dem Index der Zeitangabe kann hier der Chunkwert ausgelesen werden und als chunk_id übergeben werden
+    if ctx.triggered[0]["prop_id"] == "+_button_frontpage.n_clicks":
+        chunk_id = chunk_number +1
+    elif ctx.triggered[0]["prop_id"] == "-_button_frontpage.n_clicks":
+        chunk_id = chunk_number -1
     else:
-        chunk_id = clickData["points"][0]["x"]
+
+        if tc_indicator:
+            time_id = clickData["points"][0]["x"]
+            row_index = chronology_df.index.get_loc(chronology_df[chronology_df["minute"] == time_id].index[0]) # die Information aus dem DF aus Chronology. Hier wird die Zeit und das zugehörige DF gespeichert. Wir müssen zunächst den Index der Zeitangabe finden
+            chunk_id = chronology_df.loc[row_index]["ind"] # mit dem Index der Zeitangabe kann hier der Chunkwert ausgelesen werden und als chunk_id übergeben werden
+        else:
+            chunk_id = clickData["points"][0]["x"]
 
     sent_example = []
     speaker = "None"
@@ -391,7 +415,7 @@ def sent_drawing(clickData):
                 speaker = top_dic["korpus"][interview_id[0:3]][interview_id]["sent"][a]["speaker"]
 
     sent_id = "Chunk: " + str(chunk_id)
-    return sent_example, sent_id
+    return sent_example, sent_id, chunk_id
 
 
 # Balkendiagramm auf der ersten Seite
@@ -407,6 +431,7 @@ def bar_map(data2):
 @app.callback(
     Output(component_id="bar2", component_property="figure"),
     Input("top_dic2", "data2"),
+    prevent_initial_call=True
 )
 def bar_map(data2):
     fig = bar_dic(top_dic, show_fig = False, return_fig = True)
@@ -415,55 +440,55 @@ def bar_map(data2):
 @app.callback(
     Output("heatmap_interview_topic_nr", "data"),
     Input("heat_map_interview", "clickData"),
+    prevent_initial_call=True
 )
 def gloabl_topic_nr_update(clickData):
 
     topic = clickData["points"][0]["y"]
-    print(topic)
     return topic
 
 @app.callback(
     Output("heatmap_interview_detail_topic_nr", "data"),
     Input("heat_map_interview_detail", "clickData"),
+    prevent_initial_call=True
 )
 def gloabl_topic_nr_update(clickData):
 
     topic = clickData["points"][0]["y"]
-    print(topic)
     return topic
 
 @app.callback(
     Output("bar_topic_nr", "data"),
     Input("bar", "clickData"),
+    prevent_initial_call=True
 )
 def gloabl_topic_nr_update(clickData):
 
     topic = clickData["points"][0]["x"]
-    print(topic)
     return topic
 
 @app.callback(
     Output("bar_detail_topic_nr", "data"),
     Input("bar2", "clickData"),
+    prevent_initial_call=True
 )
 def gloabl_topic_nr_update(clickData):
 
     topic = clickData["points"][0]["x"]
-    print(topic)
     return topic
 
 @app.callback(
     Output("heatmap_corpus_topic_nr", "data"),
     Input("heat_map", "clickData"),
+    prevent_initial_call=True
 )
 def gloabl_topic_nr_update(clickData):
 
     topic = clickData["points"][0]["x"]
-    print(topic)
     return topic
 
 
-# Ausgabe der ersten 50 Worte des ausgewählten Topics
+# Ausgabe der ersten 50 Worte des ausgewählten Topics von jedem Graphen bei Click oder manueller Eingabe
 @app.callback(
     Output("topics", "children"),
     Input("input", "value"),
@@ -472,6 +497,7 @@ def gloabl_topic_nr_update(clickData):
     Input("bar_topic_nr", "data"),
     Input("bar_detail_topic_nr", "data"),
     Input("heatmap_corpus_topic_nr", "data"),
+    prevent_initial_call=True
 
 )
 def df_input(value1, value2, value3, value4, value5, value6):
@@ -486,6 +512,7 @@ def df_input(value1, value2, value3, value4, value5, value6):
 @app.callback(
     Output("topics1", "children"),
     Input("input1", "value"),
+    prevent_initial_call=True
 )
 def df_input(value):
     entry = top_words(value, top_dic)
@@ -494,6 +521,7 @@ def df_input(value):
 @app.callback(
     Output("topics2", "children"),
     Input("input2", "value"),
+    prevent_initial_call=True
 )
 def df_input(value):
     entry = top_words(value, top_dic)
@@ -502,6 +530,7 @@ def df_input(value):
 @app.callback(
     Output("topics3", "children"),
     Input("input3", "value"),
+    prevent_initial_call=True
 )
 def df_input(value):
     entry = top_words(value, top_dic)
@@ -514,6 +543,7 @@ def df_input(value):
     Input("topic_print", "value"),
     Input("weight_print", "value"),
     Input("enter_print", "n_clicks"),
+    prevent_initial_call=True
 )
 
 def weight_print(topic_print, weight_print, n_clicks):
@@ -553,6 +583,7 @@ def weight_print(topic_print, weight_print, n_clicks):
     Input("threshold_top_filter_value_detail", "value"),
     Input("outlier_threshold_value_detail", "value"),
     Input("interview_manual_id_detail", "value"),
+    prevent_initial_call=True
 )
 def interview_heat_map(heatmap_filter, top_filter_th, outlier_th, interview_manual_id):
 
@@ -592,19 +623,28 @@ def interview_heat_map(heatmap_filter, top_filter_th, outlier_th, interview_manu
 @app.callback(
     Output(component_id='textarea_detail', component_property='children'),
     Output("sent_title_detail", "children"),
+    Output("chunk_number_detail", "data"),
     Input("heat_map_interview_detail", "clickData"),
-)
-def sent_drawing_detail(clickData):
+    Input("-_button_detail", "n_clicks"),
+    Input("+_button_detail", "n_clicks"),
+    State("chunk_number_detail", "data"),
 
-    #chunk_id = clickData["points"][0]["x"]
+    prevent_initial_call=True
+)
+def sent_drawing_detail(clickData, input_before, input_next, chunk_number):
     interview_id = interview_id_detail
 
-    if tc_indicator_detail:
-        time_id = clickData["points"][0]["x"]
-        row_index = chronology_df_detail.index.get_loc(chronology_df[chronology_df_detail["minute"] == time_id].index[0]) # die Information aus dem DF aus Chronology. Hier wird die Zeit und das zugehörige DF gespeichert. Wir müssen zunächst den Index der Zeitangabe finden
-        chunk_id = chronology_df_detail.loc[row_index]["ind"] # mit dem Index der Zeitangabe kann hier der Chunkwert ausgelesen werden und als chunk_id übergeben werden
+    if ctx.triggered[0]["prop_id"] == "+_button_detail.n_clicks":
+        chunk_id = chunk_number +1
+    elif ctx.triggered[0]["prop_id"] == "-_button_detail.n_clicks":
+        chunk_id = chunk_number -1
     else:
-        chunk_id = clickData["points"][0]["x"]
+        if tc_indicator_detail:
+            time_id = clickData["points"][0]["x"]
+            row_index = chronology_df_detail.index.get_loc(chronology_df[chronology_df_detail["minute"] == time_id].index[0]) # die Information aus dem DF aus Chronology. Hier wird die Zeit und das zugehörige DF gespeichert. Wir müssen zunächst den Index der Zeitangabe finden
+            chunk_id = chronology_df_detail.loc[row_index]["ind"] # mit dem Index der Zeitangabe kann hier der Chunkwert ausgelesen werden und als chunk_id übergeben werden
+        else:
+            chunk_id = clickData["points"][0]["x"]
 
     sent_example = []
     speaker = "None"
@@ -618,7 +658,7 @@ def sent_drawing_detail(clickData):
                 speaker = top_dic["korpus"][interview_id[0:3]][interview_id]["sent"][a]["speaker"]
 
     sent_id = "Chunk: " + str(chunk_id)
-    return sent_example, sent_id
+    return sent_example, sent_id, chunk_id
 
 if __name__ == '__main__':
     app.run_server(debug=False, port=3002)
