6437666851c8559880bd8ae33867a2defe406861
Marc Altmann <marcaltmann@posteo.de>
2025-04-08 16:03:29 +0200

    Apply ruff formatting

--- functions/graph_functions/heat_maps.py
+++ functions/graph_functions/heat_maps.py
@@ -1,14 +1,21 @@
 from builtins import print
+
 import pandas as pd
 import plotly_express as px
+
 from functions.basic_functions.convert_ohtm_file import convert_ohtm_file
 
 
-def heatmap_corpus(ohtm_file, option_selected: str = "all",
-                   show_fig: bool = True, return_fig: bool = False, z_score_global: str = "True",
-                   topic_filter_number: int = 0, topic_filter_threshold: float = 0, topic_filter: str = "True"
-                   ):
-
+def heatmap_corpus(
+    ohtm_file,
+    option_selected: str = "all",
+    show_fig: bool = True,
+    return_fig: bool = False,
+    z_score_global: str = "True",
+    topic_filter_number: int = 0,
+    topic_filter_threshold: float = 0,
+    topic_filter: str = "True",
+):
     ohtm_file = convert_ohtm_file(ohtm_file)
 
     if "z_score" in z_score_global:
@@ -20,7 +27,7 @@ def heatmap_corpus(ohtm_file, option_selected: str = "all",
     else:
         topic_filter_value = False
     if option_selected == "None":
-        option_selected ="all"
+        option_selected = "all"
 
     if ohtm_file["settings"]["topic_modeling"]["trained"] == "True":
         if option_selected == "all":
@@ -33,12 +40,20 @@ def heatmap_corpus(ohtm_file, option_selected: str = "all",
                         count += 1
                         for t in ohtm_file["weight"][archive][interview][c]:
                             if t not in heat_dic[interview]:
-                                heat_dic[interview].update({t: ohtm_file["weight"][archive][interview][c][t]})
+                                heat_dic[interview].update(
+                                    {t: ohtm_file["weight"][archive][interview][c][t]}
+                                )
                             else:
                                 heat_dic[interview].update(
-                                    {t: heat_dic[interview][t] + ohtm_file["weight"][archive][interview][c][t]})
+                                    {
+                                        t: heat_dic[interview][t]
+                                        + ohtm_file["weight"][archive][interview][c][t]
+                                    }
+                                )
                     for entry in heat_dic[interview]:
-                        heat_dic[interview].update({entry: heat_dic[interview][entry] / count})
+                        heat_dic[interview].update(
+                            {entry: heat_dic[interview][entry] / count}
+                        )
         else:
             archive = option_selected
             heat_dic = {}
@@ -49,18 +64,27 @@ def heatmap_corpus(ohtm_file, option_selected: str = "all",
                     count += 1
                     for t in ohtm_file["weight"][archive][interview][c]:
                         if t not in heat_dic[interview]:
-                            heat_dic[interview].update({t: ohtm_file["weight"][archive][interview][c][t]})
+                            heat_dic[interview].update(
+                                {t: ohtm_file["weight"][archive][interview][c][t]}
+                            )
                         else:
                             heat_dic[interview].update(
-                                {t: heat_dic[interview][t] + ohtm_file["weight"][archive][interview][c][t]})
+                                {
+                                    t: heat_dic[interview][t]
+                                    + ohtm_file["weight"][archive][interview][c][t]
+                                }
+                            )
                 for entry in heat_dic[interview]:
-                    heat_dic[interview].update({entry: heat_dic[interview][entry] / count})
-
+                    heat_dic[interview].update(
+                        {entry: heat_dic[interview][entry] / count}
+                    )
 
         if topic_filter_value:
             heat_dic_2 = {}
             for interview in heat_dic:
-                if str(heat_dic[interview][str(topic_filter_number)]) >= str(topic_filter_threshold):
+                if str(heat_dic[interview][str(topic_filter_number)]) >= str(
+                    topic_filter_threshold
+                ):
                     if "e" in str(heat_dic[interview][str(topic_filter_number)]):
                         next
                     else:
@@ -71,14 +95,18 @@ def heatmap_corpus(ohtm_file, option_selected: str = "all",
         if z_score:
             mean = df.mean()
             std_dev = df.std()
-            z_scores = ((df - mean) / std_dev)
+            z_scores = (df - mean) / std_dev
             df = z_scores
 
         df = df.transpose()
-        fig = px.imshow(df, color_continuous_scale='dense', aspect='auto')
-        fig.update_traces(hovertemplate="Interview: %{y}" "<br>Topic: %{x}" "<br>Weight: %{z}<extra></extra>")
-        fig.update_layout(clickmode='event+select')
-        fig.update_layout(clickmode='event+select')
+        fig = px.imshow(df, color_continuous_scale="dense", aspect="auto")
+        fig.update_traces(
+            hovertemplate="Interview: %{y}"
+            "<br>Topic: %{x}"
+            "<br>Weight: %{z}<extra></extra>"
+        )
+        fig.update_layout(clickmode="event+select")
+        fig.update_layout(clickmode="event+select")
         fig.update(layout_coloraxis_showscale=False)
         fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
         if show_fig:
@@ -97,25 +125,27 @@ improved. (17.1.2025)
 
 
 """
-from functions.graph_functions.heatmap_marker import heatmap_marker_creation_normal
 from dash import no_update
 
+from functions.graph_functions.heatmap_marker import heatmap_marker_creation_normal
 
-def heatmap_interview_simple(ohtm_file,
-                             click_data,
-                             click_data_2,
-                             ctx_triggered,
-                             chunk_number_storage,
-                             heatmap_filter: list = "",
-                             interview_manual_id: str = "",
-                             ):
+
+def heatmap_interview_simple(
+    ohtm_file,
+    click_data,
+    click_data_2,
+    ctx_triggered,
+    chunk_number_storage,
+    heatmap_filter: list = "",
+    interview_manual_id: str = "",
+):
     interview_id = None
     if click_data == "off":
         interview_id = interview_manual_id
     else:
         if interview_manual_id is not None:
             interview_id = interview_manual_id
-            if interview_manual_id == '':
+            if interview_manual_id == "":
                 interview_id = click_data["points"][0]["y"]
         else:
             if click_data is not None:
@@ -141,12 +171,16 @@ def heatmap_interview_simple(ohtm_file,
             if z_score:
                 mean = df.mean()
                 std_dev = df.std()
-                z_scores = ((df - mean)/std_dev)
+                z_scores = (df - mean) / std_dev
                 df = z_scores
                 df.index = pd.to_numeric(df.index)
 
-            fig = px.imshow(df, color_continuous_scale='dense')
-            fig.update_traces(hovertemplate="Chunk: %{x}" "<br>Topic: %{y}" "<br>Weight: %{z}<extra></extra>")
+            fig = px.imshow(df, color_continuous_scale="dense")
+            fig.update_traces(
+                hovertemplate="Chunk: %{x}"
+                "<br>Topic: %{y}"
+                "<br>Weight: %{z}<extra></extra>"
+            )
             fig.update_traces(showlegend=False)
             fig.update_traces(showscale=False)
             fig.update(layout_coloraxis_showscale=False)
@@ -159,14 +193,22 @@ def heatmap_interview_simple(ohtm_file,
                 title = "Interview " + interview_manual_id
             else:
                 if "marker" in heatmap_filter:
-                    marker = heatmap_marker_creation_normal(click_data_2, chunk_number_storage)
+                    marker = heatmap_marker_creation_normal(
+                        click_data_2, chunk_number_storage
+                    )
                     fig.add_vrect(
-                        x0=marker[0], x1=marker[1],
-                        fillcolor="LightSalmon", opacity=0.3,
-                        layer="above", line_width=1, )
+                        x0=marker[0],
+                        x1=marker[1],
+                        fillcolor="LightSalmon",
+                        opacity=0.3,
+                        layer="above",
+                        line_width=1,
+                    )
                 title = "Interview " + interview_id
 
-            df = df.to_json() # dash only can store json, so this df has to be converted
+            df = (
+                df.to_json()
+            )  # dash only can store json, so this df has to be converted
             return fig, title, interview_id, chunk_number_storage, tc_indicator, df
 
         else:
